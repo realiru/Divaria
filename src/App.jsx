411706282726888a2c9cd51f9a906273b5b1d1e3
Hamzah Patel <hamzahpatel403@gmail.com>
2025-12-06 23:53:57 +1300

    Rough Website
    Created a rough idea for the website and reformatted the files to be more inline with Vite.
    
    Todo:
    - Fix colour
    - Fix sizing
    - Complete footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <div>
-        
+    <div className="hero">
+      <div className="hero-content">
+        <div className="hero-logo">
+          <img src="/Divaria logo.png" alt="Divaria Logo" />
+        </div>
+
+        <h1 className="hero-title">Lorem ipsum dolor sit amet<br />Lorem ips</h1>
+
+        <p className="hero-body">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit.
+          Quisque faucibus ex sapien vitae pellentesque sem placerat.
+          In id cursus mi pretium tellus duis convallis. Tempus leo eu
+          aenean sed diam urna tempor.
+        </p>
+
+        <p className="hero-body small">
+          Ad litora torquent per conubia nostra inceptos himenaeos.
+        </p>
+
+        <a href="#" className="hero-button">Shop on Trade Me</a>
       </div>
+    </div>
+
+    <footer className="footer">
+      <p>&copy; 2025 Divaria. All rights reserved.</p>
+    </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
