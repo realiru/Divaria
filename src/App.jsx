4f7b25a4bdd599ef124a187442d96e13367a3c28
Hamzah Patel <hamzahpatel403@gmail.com>
2025-12-12 12:09:37 +1300

    Final corrections
    Update button to link to correct trade me page (check for mobile web included to direct to trade me app instead)
    Placeholder text replaced
    Made logo smaller by 25%
    Updated contact info
    Made background image shift from the right center on smaller displays

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
 
 function App() {
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  const tradeMeUrl = isMobile 
+    ? "https://www.trademe.co.nz/a/marketplace/stores/Divaria"
+    : "https://www.trademe.co.nz/stores/Divaria";
+
   return (
     <>
     <div className="hero">
@@ -9,20 +14,14 @@ function App() {
           <img src="/Divaria logo.png" alt="Divaria Logo" />
         </div>
 
-        <h1 className="hero-title">Lorem ipsum dolor sit amet<br />Lorem ips</h1>
-
         <p className="hero-body">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit.
-          Quisque faucibus ex sapien vitae pellentesque sem placerat.
-          In id cursus mi pretium tellus duis convallis. Tempus leo eu
-          aenean sed diam urna tempor.
+          I have a passion for Vintage and Antique jewellery.<br />
+          From restoring them, researching their past and unlocking their story.<br />
+          Each piece is unique, valuable and handcrafted.<br />
+          Discover the Divaria difference now
         </p>
 
-        <p className="hero-body small">
-          Ad litora torquent per conubia nostra inceptos himenaeos.
-        </p>
-
-        <a href="#" className="hero-button">Shop on Trade Me</a>
+        <a href={tradeMeUrl} className="hero-button" target="_blank" rel="noreferrer">Shop on Trade Me</a>
       </div>
     </div>
 
@@ -53,7 +52,7 @@ function App() {
                 <path fillRule="evenodd" clipRule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" stroke="currentColor" strokeWidth="1.5" fill="none" />
               </svg>
             </a>
-            <a className="footer-icon" href="mailto:info@divaria.com" aria-label="Email">
+            <a className="footer-icon" href="mailto:theblacksnz@gmail.com" aria-label="Email">
               <svg viewBox="0 0 24 24" width="32" height="32" fill="none" aria-hidden="true">
                 <path
                   d="M21 8 17.4392 9.97822C15.454 11.0811 14.4614 11.6326 13.4102 11.8488C12.4798 12.0401 11.5202 12.0401 10.5898 11.8488C9.53864 11.6326 8.54603 11.0811 6.5608 9.97822L3 8"
@@ -71,7 +70,7 @@ function App() {
                 />
               </svg>
             </a>
-            <a className="footer-icon" href="tel:+640000000" aria-label="Call">
+            <a className="footer-icon" href="tel:+640212406432" aria-label="Call">
               <svg viewBox="0 0 24 24" width="32" height="32" fill="none" aria-hidden="true">
                 <path
                   d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z"
@@ -83,7 +82,7 @@ function App() {
               </svg>
             </a>
           </div>
-          <p className="footer-phone">+64 00000000</p>
+          <p className="footer-phone">0212406432</p>
         </div>
       </div>
     </footer>
